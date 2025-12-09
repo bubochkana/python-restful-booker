df08efc580e8a735455db4d0144db988a1057436
Anna Voitiuk <avoitiuk@softserveinc.com>
2025-12-09 16:34:22 +0300

    added config and config parser

---
+++ core/utils/booker_config_parser.py
@@ -0,0 +1,32 @@
+import configparser
+from pathlib import Path
+
+configFileDir = 'config'
+configFile = 'restful_booker_config.ini'
+
+config = configparser.ConfigParser()
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+CONFIG_FILE = BASE_DIR.joinpath(configFileDir).joinpath(configFile)
+
+config.read(CONFIG_FILE)
+
+
+def get_restful_booker_api_url():
+    return config['restful-booker']['url']
+
+
+def get_booking_ids_api_url():
+    return get_restful_booker_api_url() + config['resources']['booking']
+
+
+def get_auth_url():
+    return get_restful_booker_api_url() + config['resources']['auth']
+
+
+def get_username():
+    return config['authentication']['username']
+
+
+def get_password():
+    return config['authentication']['password']

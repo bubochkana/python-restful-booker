a18d71f48d97a88e824d07ef36959fcb01735a8e
Anna Voitiuk <avoitiuk@softserveinc.com>
2026-01-14 14:55:13 +0200

    added one more booking e2e test

--- tests/booking/test_booking_e2e.py
+++ tests/booking/test_booking_e2e.py
@@ -41,10 +41,8 @@ class TestBookingE2E:
         booking_endpoint.delete_booking(booking_id)
 
         # get the booking
-        get_booking_response_after_deletion = (booking_endpoint
-                                               .get_booking_by_id(booking_id))
-        (assert_that(get_booking_response_after_deletion.status_code)
-         .is_equal_to(requests.codes.not_found))
+        get_booking_response_after_deletion = booking_endpoint.get_booking_by_id(booking_id)
+        assert_that(get_booking_response_after_deletion.status_code).is_equal_to(requests.codes.not_found)
 
     def test_booking_create_update_e2e(self):
         client = BookingClient()
@@ -52,9 +50,9 @@ class TestBookingE2E:
 
         # create a booking
         create_request_body = booking_endpoint.build_random_booking()
-        create_booking_response = (booking_endpoint.create_booking(
-            create_request_body))
+        create_booking_response = booking_endpoint.create_booking(create_request_body)
         booking_model = CreateBookingResponse(**create_booking_response.json())
+
         comparison_results = (CompareModel().compare_values(
             create_request_body.model_dump(),
             booking_model.booking.model_dump()))
@@ -74,6 +72,16 @@ class TestBookingE2E:
                     f"{comparison_results}").is_empty()
 
         # update (patch) booking
+        update_request_body = booking_endpoint.build_random_booking()
+        update_booking_response = booking_endpoint.update_booking(booking_id, update_request_body)
+        update_booking_model = BookingModel(**update_booking_response.json())
+
+        comparison_results = (CompareModel().compare_values(
+            update_request_body.model_dump(),
+            update_booking_model.model_dump()))
+        assert_that(comparison_results,
+                    f"Following differences found: "
+                    f"{comparison_results}").is_empty()
 
         # get the booking after update
         get_booking_response = (booking_endpoint
@@ -81,7 +89,7 @@ class TestBookingE2E:
 
        # compare created and updated (patched) bookings
         comparison_results = (CompareModel().compare_values(
-            create_request_body.model_dump(),
+            update_request_body.model_dump(),
             get_booking_response.json()))
         assert_that(comparison_results,
                     f"Following differences found: "

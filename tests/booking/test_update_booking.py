c7031de566aec26584d71a4425e7761cf1f7a95b
Anna Voitiuk <avoitiuk@softserveinc.com>
2026-01-15 22:31:25 +0200

    fixed merge conflicts

--- tests/booking/test_update_booking.py
+++ tests/booking/test_update_booking.py
@@ -7,31 +7,23 @@ from src.helpers.compare_models import CompareModel
 
 class TestUpdateBooking:
     def test_update_booking(self):
+        booking_id_to_update = BookingClient().booking_endpoint().pick_random_booking_id()
 
-        booking_id_to_update = (BookingClient()
-                                .booking_endpoint()
-                                .pick_random_booking_id())
+        random_booking = BookingClient().booking_endpoint().build_random_booking()
+        updated_booking = BookingClient().booking_endpoint().update_booking(booking_id_to_update, random_booking)
 
-        random_booking = (BookingClient().booking_endpoint()
-                          .build_random_booking())
-        updated_booking = (BookingClient()
-                           .booking_endpoint()
-                           .update_booking(
-            booking_id_to_update, random_booking))
-
-        comparison_results = (CompareModel.compare_values(
-            random_booking.model_dump(), updated_booking.json()))
-        assert_that(comparison_results,
-                    f"Following differences found: "
-                    f"{comparison_results}").is_empty()
+        comparison_results = CompareModel.compare_values(random_booking.model_dump(), updated_booking.json())
+        assert_that(comparison_results, f"Following differences found: {comparison_results}").is_empty()
 
     def test_update_booking_no_auth_header(self):
-        booking_id_to_update \
-            = (BookingClient().booking_endpoint()
-               .pick_random_booking_id())
-        response = BookingClient().booking_endpoint().update_booking(
-            booking_id_to_update,
-            BookingClient().booking_endpoint().build_random_booking(),
-            headers={"Content-Type": "application/json",
-                     "Accept": "application/json"})
+        booking_id_to_update = BookingClient().booking_endpoint().pick_random_booking_id()
+        response = (
+            BookingClient()
+            .booking_endpoint()
+            .update_booking(
+                booking_id_to_update,
+                BookingClient().booking_endpoint().build_random_booking(),
+                headers={"Content-Type": "application/json", "Accept": "application/json"},
+            )
+        )
         assert_that(response.status_code).is_equal_to(requests.codes.forbidden)
